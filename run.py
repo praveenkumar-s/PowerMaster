6687aa23eb6045371faeab53a728350c07408472
praveentechnic <praveentechnic@gmail.com>
2018-12-16 10:06:24 +0530

    trial

--- run.py
+++ run.py
@@ -1,34 +1,55 @@
+import time
 import pigpio
+from datetime import datetime
+import config
+import subprocess
+from datetime import datetime
+import os
+from Firebase_Client_Library import firebase_client
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-
+import data_model
+import json
 try:
     import asyncio
 except ImportError:
     import trollius as asyncio
 
-    
+
+
+
+
+timediff=datetime.now()
+counter=0
+IMP=0
+
+# 1200 imp / hour 
+# current wattage usage = 3600/ (time interval * 1200)
+#call back function, that gets triggered every time there is a pulse
 def cbf(gpio, level, tick):
-       print(gpio, level, tick)
-
-
+    global IMP
+    IMP=IMP+1
+    print("{:2d}->{} at {}".format(gpio, level, tick))
+    
 GPIO=15
 GLITCH=500
 SLEEP=60
 
 pi = pigpio.pi()
 
-pi = pigpio.pi()
-
 if not pi.connected:
    exit(0)
+
 pi.set_mode(GPIO, pigpio.INPUT)
 
+# Ignore edges shorter than GLITCH microseconds.
 pi.set_glitch_filter(GPIO, GLITCH)
-
 
 cb = pi.callback(GPIO, pigpio.FALLING_EDGE, cbf)
 
+
+print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+# Execution will block here until Ctrl+C (Ctrl+Break on Windows) is pressed.
 try:
     asyncio.get_event_loop().run_forever()
 except (KeyboardInterrupt, SystemExit):
